ff4e2f358f0a8c03d156e27630fafd9db03b19d6
dkuroi <kaerit@hotmail.com>
2021-09-25 01:10:12 +0100

    Parte 1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
+// @ is an alias to /src
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  @import "@/styles/grid-default.scss";
+  @import "@/styles/grid-xs.scss";
+  @import "@/styles/grid-sm.scss";
+  @import "@/styles/grid-md.scss";
+  @import "@/styles/grid-lg.scss";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  body {
+    background: #030303 url("~@/assets/bg.gif");
+    background-position-x: center;
+    background-size: cover;
+    min-height: 97.77617926577vh;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
   }
-}
+
+  ::-webkit-scrollbar { // fondo
+    background: #010101;
+    border: 3px solid #010101;
+  }
+  ::-webkit-scrollbar-button { // botones arriba y abajo
+    color: white;
+    border-radius: 5px;
+    background: #0c0c0c;
+    border: 3px solid #010101;
+  }
+  ::-webkit-scrollbar-thumb { // dragger
+    background: #0c0c0c;
+    border: 3px solid #010101;
+  }
+
+
+  // ESTILOS TESTING
+  .bgteal { background: teal; }
+  .bgviol { background: blueviolet; }
+  .bgyell { background: #E5bf5d; }
+  .bgdark { background: #0a0a0a; color: white }
+  .bglight { background: gainsboro; }
+
+  .box-high {
+    display: block;
+    height: 100px;
+    padding: 10px;
+  }
+  
 </style>
