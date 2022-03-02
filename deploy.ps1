e867f61598e98cca394c8b1b868f4fe191c7d0ad
Diego Vázquez Campos <kaerit@hotmail.com>
2022-03-02 16:58:28 +0000

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -2,4 +2,4 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:dkaerit/oc3d-directory.git master:gh-pages
+git push -f git@github.com:dkaerit/oc-directory.git master:gh-pages
